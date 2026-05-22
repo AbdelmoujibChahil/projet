1d5c73ac95b38abe2c4b84294fb0cd52904ce1ac
Moujib <Moujibch10@gmail.com>
2026-05-22 22:07:03 +0100

    refactor(user): refactoring user controller

---
+++ app/Features/User/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+namespace App\Features\User\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Features\User\Services\UserService;
+use App\Features\User\Requests\StoreUserRequest;
+use App\Features\User\Requests\UpdateUserRequest;
+use App\Features\User\Resources\UserResource;
+use Illuminate\Http\Request;
+class UserController extends Controller
+{
+    public function allUsers(UserService $service)
+    {
+        return response()->json($service->getAllUsersWithStats());
+    }
+
+    public function store(StoreUserRequest $request, UserService $service)
+    {
+         $user = $service->create($request->validated());
+        return response()->json(new UserResource($user), 201);
+    }
+
+    public function updateProfile(UpdateUserRequest $request, UserService $service)
+    {
+        return response()->json($service->update(auth()->user(), $request->validated()));
+    }
+
+    public function updatePassword(Request $request, UserService $service)
+    {
+        return response()->json($service->updatePassword(auth()->user(), $request->password));
+    }
+
+    public function updatePhone(Request $request, UserService $service)
+    {
+        return response()->json($service->updatePhone(auth()->user(), $request->phone));
+    }
+}
