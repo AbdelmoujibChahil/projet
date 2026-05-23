435d1f11c7946b7a29b167fb9bbf5c563c527e16
Moujib <Moujibch10@gmail.com>
2026-05-23 18:15:17 +0100

    refactor(order): clean order API endpoints

---
+++ routes/api/orders.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Features\Order\Controllers\OrderController;
+
+/* USER ROUTES */
+
+Route::middleware('auth:api')
+    ->prefix('orders')
+    ->group(function () {
+
+        // Create order
+        Route::post('/', [OrderController::class, 'store']);
+
+        // Client orders
+        Route::get('/client', [
+            OrderController::class,
+            'getClientOrders'
+        ]);
+
+    });
+
+/* ADMIN ROUTES*/
+
+Route::middleware(['auth:api', 'admin'])
+    ->prefix('admin/orders')
+    ->group(function () {
+
+        // All orders
+        Route::get('/', [
+            OrderController::class,
+            'index'
+        ]);
+        // Update status
+        Route::patch('/{commande}', [
+            OrderController::class,
+            'updateStatus'
+        ]);
+
+    });
