bcc173fa2befb9bb8991da43f9515c95f1364c89
Moujib <Moujibch10@gmail.com>
2026-05-29 21:03:29 +0100

    feat(payment): add payment model

---
+++ app/Models/Payment.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'commande_id',
+        'payment_method',
+        'status',
+        'transaction_id',
+        'amount'
+    ];
+
+    public function commande()
+    {
+        return $this->belongsTo(Commande::class);
+    }
+}
